ade9561a8f9757e4c99e99d582abf59fcb1f9da6
th3gumm3l <Tim_Bernhard@sluz.ch>
2022-01-17 09:03:09 +0100

    final patch

--- IDAF Quadratische Gleichungen/Form1.Designer.cs
+++ IDAF Quadratische Gleichungen/Form1.Designer.cs
@@ -28,13 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btn_berechnen = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.tbx_EingabeC = new System.Windows.Forms.TextBox();
+            this.tbx_EingabeB = new System.Windows.Forms.TextBox();
+            this.tbx_EingabeA = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbx_AusgabeX1 = new System.Windows.Forms.TextBox();
+            this.tbx_AusgabeX2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.grResBox = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.grResBox);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 350);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btn_berechnen);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(546, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(254, 100);
+            this.panel4.TabIndex = 0;
+            // 
+            // btn_berechnen
+            // 
+            this.btn_berechnen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_berechnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_berechnen.Location = new System.Drawing.Point(0, 0);
+            this.btn_berechnen.Name = "btn_berechnen";
+            this.btn_berechnen.Size = new System.Drawing.Size(254, 100);
+            this.btn_berechnen.TabIndex = 0;
+            this.btn_berechnen.Text = "Berechnen";
+            this.btn_berechnen.UseVisualStyleBackColor = true;
+            this.btn_berechnen.Click += new System.EventHandler(this.btn_berechnen_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel7);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 100);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(800, 250);
+            this.panel5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IDAF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quadratische Gleichungen";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label7);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Controls.Add(this.tbx_AusgabeX2);
+            this.panel6.Controls.Add(this.tbx_AusgabeX1);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Controls.Add(this.tbx_EingabeC);
+            this.panel6.Controls.Add(this.tbx_EingabeB);
+            this.panel6.Controls.Add(this.tbx_EingabeA);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(540, 250);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.richTextBox1);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel7.Location = new System.Drawing.Point(546, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(254, 250);
+            this.panel7.TabIndex = 1;
+            // 
+            // tbx_EingabeC
+            // 
+            this.tbx_EingabeC.Location = new System.Drawing.Point(57, 166);
+            this.tbx_EingabeC.Name = "tbx_EingabeC";
+            this.tbx_EingabeC.Size = new System.Drawing.Size(100, 20);
+            this.tbx_EingabeC.TabIndex = 5;
+            // 
+            // tbx_EingabeB
+            // 
+            this.tbx_EingabeB.Location = new System.Drawing.Point(57, 108);
+            this.tbx_EingabeB.Name = "tbx_EingabeB";
+            this.tbx_EingabeB.Size = new System.Drawing.Size(100, 20);
+            this.tbx_EingabeB.TabIndex = 4;
+            // 
+            // tbx_EingabeA
+            // 
+            this.tbx_EingabeA.Location = new System.Drawing.Point(57, 54);
+            this.tbx_EingabeA.Name = "tbx_EingabeA";
+            this.tbx_EingabeA.Size = new System.Drawing.Size(100, 20);
+            this.tbx_EingabeA.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(254, 250);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Geben Sie links die Werte A,B und C ein und lassen sie sich das Resultat berechne" +
+    "n.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Geben Sie A ein:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Geben Sie B ein:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Geben Sie C ein:";
+            // 
+            // tbx_AusgabeX1
+            // 
+            this.tbx_AusgabeX1.Location = new System.Drawing.Point(339, 81);
+            this.tbx_AusgabeX1.Name = "tbx_AusgabeX1";
+            this.tbx_AusgabeX1.ReadOnly = true;
+            this.tbx_AusgabeX1.Size = new System.Drawing.Size(110, 20);
+            this.tbx_AusgabeX1.TabIndex = 9;
+            // 
+            // tbx_AusgabeX2
+            // 
+            this.tbx_AusgabeX2.Location = new System.Drawing.Point(339, 140);
+            this.tbx_AusgabeX2.Name = "tbx_AusgabeX2";
+            this.tbx_AusgabeX2.ReadOnly = true;
+            this.tbx_AusgabeX2.Size = new System.Drawing.Size(110, 20);
+            this.tbx_AusgabeX2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(336, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Das Resultat für x1 ist:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(336, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Das Resultat für x2 ist:";
+            // 
+            // grResBox
+            // 
+            this.grResBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grResBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grResBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grResBox.Location = new System.Drawing.Point(0, 0);
+            this.grResBox.Name = "grResBox";
+            this.grResBox.ReadOnly = true;
+            this.grResBox.Size = new System.Drawing.Size(546, 100);
+            this.grResBox.TabIndex = 1;
+            this.grResBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IDAF Quadratische Gleichungen";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox tbx_EingabeC;
+        private System.Windows.Forms.TextBox tbx_EingabeB;
+        private System.Windows.Forms.TextBox tbx_EingabeA;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btn_berechnen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbx_AusgabeX2;
+        private System.Windows.Forms.TextBox tbx_AusgabeX1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox grResBox;
     }
 }
 
--- IDAF Quadratische Gleichungen/Form1.cs
+++ IDAF Quadratische Gleichungen/Form1.cs
@@ -16,5 +16,33 @@ namespace IDAF_Quadratische_Gleichungen
         {
             InitializeComponent();
         }
+
+        private void btn_berechnen_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(tbx_EingabeA.Text);
+            double b = Convert.ToDouble(tbx_EingabeB.Text);
+            double c = Convert.ToDouble(tbx_EingabeC.Text);
+            
+            if (Math.Pow(b, 2)-4 * a * c == 0)
+            {
+                grResBox.Text = "Es gibt nur eine Lösung";
+                MessageBox.Show("Es gibt nur eine Lösung");
+                tbx_AusgabeX1.Text = "0";
+                tbx_AusgabeX2.Text = Convert.ToString(-b / (2 * a));
+            }
+            else if (Math.Pow(b, 2) - 4 * a * c > 0)
+            {
+                grResBox.Text = "Es gibt zwei Lösungen";
+                tbx_AusgabeX1.Text = Convert.ToString(((-b) + Math.Sqrt((Math.Pow(b, 2) - 4 * a * c))) / (2 * a));
+                tbx_AusgabeX2.Text = Convert.ToString(((-b) - Math.Sqrt((Math.Pow(b, 2) - 4 * a * c))) / (2 * a));
+            }
+            else
+            {
+                grResBox.Text = "Es gibt keine Lösung";
+                MessageBox.Show("Es gibt keine Lösung");
+                tbx_AusgabeX1.Text = "0";
+                tbx_AusgabeX2.Text = "0";
+            }
+        }
     }
 }
